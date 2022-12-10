0c7888c7f2a687d47cbf1d1b68a594595da95d67
ninefloor <less0805@gmail.com>
2022-12-10 21:48:09 +0900

    스타일지정 - 일부 page Input 컴포넌트로 변경

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,15 +1,16 @@
 import React from "react";
 import giftbeanLogo from "../Logo.png";
+import {TextInput, TextInputCenter} from "../components/styledComponents/Input"
 
 function SignUpPage() {
   return (
     <div>
       SignUpPage <img alt="Logo" src={giftbeanLogo}></img>
       <div>
-        <input placeholder="이름" />
-        <input placeholder="이메일" />
-        <input placeholder="비밀번호" />
-        <input placeholder="비밀번호 확인" />
+        <TextInputCenter placeholder="이름" />
+        <TextInputCenter type="email" placeholder="이메일" />
+        <TextInputCenter type="password" placeholder="비밀번호" />
+        <TextInputCenter placeholder="비밀번호 확인" />
       </div>
       <button>가입하기</button>
     </div>
