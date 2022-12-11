13d50ac9a02e3008c904ab21b4073d727789dff6
ninefloor <less0805@gmail.com>
2022-12-11 18:53:50 +0900

    스타일 지정 - Item 컴포넌트 생성 및 GifticonItem 컴포넌트 수정

--- src/components/GifticonItem.js
+++ src/components/GifticonItem.js
@@ -1,8 +1,9 @@
 import GifticonDetailModal from "./GifticonDetailModal";
 import { useState } from "react";
 import styled from "styled-components";
+import Item from "./styledComponents/Item";
 
-export const GifticonItemContainer = styled.button`
+export const GifticonItemContainer = styled.ul`
   width: 100%;
   height: 100%;
   background-color: red;
@@ -11,43 +12,19 @@ export const GifticonItemContainer = styled.button`
   justify-content: center;
 `;
 
-export const ModalBtn = styled.button`
-  width: 230px;
-  padding: 20px;
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  background-color: purple;
-  color: white;
-  text-decoration: none;
-  border: none;
-  border-radius: 30px;
-  cursor: pointer;
-`;
-
 function GifticonItem() {
   const [isOpen, setIsOpen] = useState(false);
 
   const openModalHandler = () => {
-    isOpen ? setIsOpen(false) : setIsOpen(true);
+    setIsOpen(!isOpen);
   };
 
   return (
     <GifticonItemContainer>
-      {/* ToDo: ModalBtn이라고 이름을 그대로 둬도 될까..? 나름 GifticonItem인데... */}
-      <ModalBtn onClick={openModalHandler}>
-        {isOpen ? (
-          <GifticonDetailModal openModalHandler={openModalHandler} />
-        ) : null}
-        <div>banner</div>
-        <div>title</div>
-        <div>pic</div>
-        <div>name</div>
-        <div>date</div>
-      </ModalBtn>
+      {isOpen ? (
+        <GifticonDetailModal openModalHandler={openModalHandler} />
+      ) : null}
+      <Item modalHandler={openModalHandler} />
     </GifticonItemContainer>
   );
 }
