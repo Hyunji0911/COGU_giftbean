684314b3a5d71b6551ec82e51a9a6c29bc3bf01d
문재웅 <answodnd112@gmail.com>
2022-12-12 22:57:26 +0900

    로그인페이지 단위컴포넌트 적용

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,18 +1,43 @@
 import React from "react";
 import Logo from "../assets/Logo.svg";
+import {
+  TextInput,
+  TextInputCenter,
+} from "../components/styledComponents/Input";
+import {
+  GreenButton,
+  GreenBottomButton,
+} from "../components/styledComponents/GreenButton";
+import styled from "styled-components";
 
+const LoginContainer = styled.div`
+  width: 390px;
+  height: 844px;
+  border: solid 1px blue;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`;
 
 function LoginPage() {
   return (
     <main>
-      <img alt="Logo" src={Logo}></img>
-      <div>
-        <input placeholder="example@mail.com" />
-        <input placeholder="password" />
-      </div>
+      <LoginContainer>
+        <img alt="Logo" src={Logo}></img>
+        <div>
+          <TextInputCenter type="email" placeholder="이메일" />
+          <TextInputCenter type="password" placeholder="비밀번호" />
+          <button>
+            <GreenButton> 이메일로 로그인하기 </GreenButton>{" "}
+          </button>
+          <button>
+            <GreenButton> 처음이신가요? </GreenButton>
+          </button>
+        </div>
+      </LoginContainer>
     </main>
   );
 }
 export default LoginPage;
-
-
