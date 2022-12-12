38980ee2c0817239e95d30a2a5cf8c3dc8b38dcc
ymymmz9 <ymymmz9@gmail.com>
2022-12-13 01:00:41 +0900

    회원가입페이지에 단위 컴포넌트 적용, 로딩페이지에 로딩인디케이터 구현

--- src/components/LoadingIndicator.js
+++ src/components/LoadingIndicator.js
@@ -1,6 +1,30 @@
 import React from "react";
-import styled from "styled-components";
+import styled,{ keyframes } from "styled-components";
 
+const spinner = keyframes`
+    from {
+    transform: rotate(0deg);
+  } to {
+    transform: rotate(360deg);
+  }
+`
+
+const Spin = styled.div `
+  
+  margin: 100px auto 0 auto; ;
+  width: 60px;
+  height: 60px;
+  border: 20px solid rgba(137, 191, 21, 0.2);
+  border-top: 20px solid rgba(137, 191, 21, 1);
+  border-radius: 50%;
+  animation: ${spinner} 1s linear infinite;
+`
 export default function LoadingIndicator() {
-  return <div>LoadingIndicator</div>;
+  return( 
+  <div>
+    
+    
+    <Spin></Spin>
+  </div>
+  )
 }
