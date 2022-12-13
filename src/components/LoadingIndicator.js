4529033267c17357828db3bbe290ee600ada8108
ymymmz9 <ymymmz9@gmail.com>
2022-12-13 13:59:37 +0900

    [Second Try]회원가입페이지에 최소 단위 컴포논트 적용, 로딩페이지에  로딩인디케이터 구현

--- src/components/LoadingIndicator.js
+++ src/components/LoadingIndicator.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled,{ keyframes } from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const spinner = keyframes`
     from {
@@ -7,24 +7,21 @@ const spinner = keyframes`
   } to {
     transform: rotate(360deg);
   }
-`
+`;
 
-const Spin = styled.div `
-  
-  margin: 100px auto 0 auto; ;
+const Spin = styled.div`
+  margin: 100px auto 0 auto;
   width: 60px;
   height: 60px;
   border: 20px solid rgba(137, 191, 21, 0.2);
   border-top: 20px solid rgba(137, 191, 21, 1);
   border-radius: 50%;
   animation: ${spinner} 1s linear infinite;
-`
+`;
 export default function LoadingIndicator() {
-  return( 
-  <div>
-    
-    
-    <Spin></Spin>
-  </div>
-  )
+  return (
+    <div>
+      <Spin></Spin>
+    </div>
+  );
 }
--- src/pages/LoadingPage.js
+++ src/pages/LoadingPage.js
@@ -1,21 +1,21 @@
-import {React,useState} from "react";
+import { React, useState } from "react";
 import styled from "styled-components";
 import LoadingIndicator from "../components/LoadingIndicator";
 import Logo from "../assets/Logo.svg";
 const LoadingPageWrapper = styled.div`
   width: 390px;
   height: 844px;
-  display:flex;
-  flex-direction:column;
-  padding-top:280px;
-`
+  display: flex;
+  flex-direction: column;
+  padding-top: 280px;
+`;
 
 function LoadingPage() {
-  const [isLoading, setIsLoading] = useState(false); 
+  const [isLoading, setIsLoading] = useState(false);
   return (
     <LoadingPageWrapper>
       <img alt="Logo" src={Logo}></img>
-      <LoadingIndicator /> 
+      <LoadingIndicator />
       {/* {isLoading ? <LoadingIndicator /> : <LoadingIndicator />} */}
     </LoadingPageWrapper>
   );
